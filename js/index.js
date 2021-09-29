363e30fec933f12de71b0ffbdfaa47af7a96e15c
Aramayis331 <86958416+Aramayis331@users.noreply.github.com>
2021-09-30 00:41:26 +0400

    add login registracia

--- js/index.js
+++ js/index.js
@@ -131,3 +131,108 @@ if(div_btn_onas && onas_more) {
 		})
 	})
 }
+
+
+// eye  //
+let eye_span = document.querySelectorAll('.eye_span');
+let password_input_log = document.querySelectorAll('.password_input');
+
+if(eye_span && password_input_log) {
+	eye_span.forEach((el, i) => {
+		el.addEventListener('click', () => {
+			if(password_input_log[i].type === 'password') {
+				password_input_log[i].type = "text";
+				el.querySelector('.hide1').style.display = 'block';
+				el.querySelector('.hide2').style.display = 'none';
+			}
+			else {
+				password_input_log[i].type = 'password';
+				el.querySelector('.hide1').style.display = 'none';
+				el.querySelector('.hide2').style.display = 'block';
+			}
+		})
+	})
+}
+
+
+//  Loin Registracia  //
+let sigin = document.getElementById('sigin');
+let sigin_nik = document.getElementById('sigin_nik');
+
+let login_input_log = document.getElementById('login_input_log');
+let password_input_logg = document.getElementById('password_input_log');
+
+let btn_sign_in = document.getElementById('btn_sign_in');
+let registracia_btn = document.getElementById('registracia_btn');
+
+let full_name_input_reg = document.getElementById('full_name_input_reg');
+let nik_input_reg = document.getElementById('nik_input_reg');
+let login_input_reg = document.getElementById('login_input_reg');
+let password_input_reg = document.getElementById('password_input_reg');
+let password_povtor_input_reg = document.getElementById('password_povtor_input_reg');
+
+let spasibo_reg = document.getElementById('spasibo_reg');
+let parol_nesovpadaet = document.getElementById('parol_nesovpadaet');
+let neverni_pass_log = document.getElementById('neverni_pass_log');
+
+registracia_btn.addEventListener('click', function() {
+	register = {
+		'fullName' : full_name_input_reg.value,
+		'nik' : nik_input_reg.value,
+		'login' : login_input_reg.value,
+		'password' : password_input_reg.value,
+		'passwordRepeat' : password_povtor_input_reg.value
+	}
+
+		let i = localStorage.length;
+		if(i <= localStorage.length) {
+			let registerString = JSON.stringify(register);
+			localStorage.setItem('test' + i, registerString);
+		}
+		if (password_input_reg.value != password_povtor_input_reg.value){
+			spasibo_reg.style.display = 'none';
+			parol_nesovpadaet.style.display = 'block';
+		}else {
+			spasibo_reg.style.display = 'block';
+			parol_nesovpadaet.style.display = 'none';
+
+			full_name_input_reg.value = '';
+			nik_input_reg.value = '';
+			login_input_reg.value = '';
+			password_input_reg.value = '';
+			password_povtor_input_reg.value = '';
+		}
+}) 
+
+btn_sign_in.addEventListener('click', function() {
+	for(let j = 0;j <= localStorage.length; j++) {
+		if(localStorage.key(j) !== null) {
+			let getStorage = localStorage.getItem(localStorage.key(j))
+			let getObject = JSON.parse(getStorage);
+			if((login_input_log.value === getObject.login) && (password_input_logg.value === getObject.password)) {
+				neverni_pass_log.style.display = 'none';
+				sigin.style.display = 'none';
+				sigin_nik.innerHTML = getObject.nik;
+				sigin_nik.style.display = 'block';
+				break;
+			}
+			
+		}
+		else {
+			neverni_pass_log.style.display = 'block'
+			login_input_log.value = '';
+			password_input_logg.value = '';
+			break;
+		}
+	}
+})
+
+
+let myModal1 = new bootstrap.Modal(document.getElementById('modalLogin'), {
+	keyboard: false,
+	backdrop: 'static'
+})
+let myModal2 = new bootstrap.Modal(document.getElementById('modalRegistr'), {
+	keyboard: false,
+	backdrop: 'static'
+})
